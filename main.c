60c8d9570e3ade7dfa218aea1365f8347e2b7e0f
Rodrigo Lima <rodrigo-l-c@hotmail.com>
2023-07-17 19:37:54 -0300

    Add main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+    Código desenvolvido no curso:
+    "C: conhecendo a Linguagem das Linguagens"
+    pela Alura...
+*/
+
+int main() {
+
+    printf("Bem vindo ao jogo da Advinhação");
+
+}
